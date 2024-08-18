ed6f56566376660dfd3f27464fe08950e127c454
pedro paulo <pedro.ferreira2@escolar.ifrn.edu.br>
2024-08-18 17:29:14 -0300

    correção questão 1

--- lista04.py
+++ lista04.py
@@ -1,13 +1,16 @@
 #questão 1
 quantidade_par = 0
 lista = []
-for x in range(3):
+for x in range(10):
     valor = int(input("insira 10 valores:"))
     lista.append (valor)
 for item in lista:
-    if item%2 == 0:
+    if item%2 == 0 and item !=0:
         quantidade_par += 1
-print(f"você digitou {quantidade_par} valores pares.")
+if quantidade_par == 1:
+    print(f"você digitou {quantidade_par} valor par.")
+else:
+    print(f"você digitou {quantidade_par} valores pares.")
 print("fim do programa.")
 
     
