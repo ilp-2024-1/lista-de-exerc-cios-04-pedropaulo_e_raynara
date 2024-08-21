4ad1eae091c43d597095d3f8dd25dec93eacc06d
Raynara <raynara.gustavo@escolar.ifrn.edu.br>
2024-08-21 18:57:27 -0300

    questão 05

--- teste_ray.py
+++ teste_ray.py
@@ -1,9 +1,12 @@
-pares = 0
+quantidade_valores = int(input('Digite a quantidade de valores que serão fornecidos para análise: '))
 lista = []
-for i in range(10):
-    valores = int(input('Digite um valor inteiro: '))
+for i in range(quantidade_valores):
+    valores = int(input('Digite um valor para ser analisado: '))
     lista.append(valores)
-    if valores % 2 == 0:
-        pares += 1
-print(f'Quantidade de valores par: {pares} ')
+    menor_valor = min(lista)
+    maior_valor = max(lista)
+    media_aritmetica = sum(lista)//quantidade_valores
+print(f'Menor valor: {menor_valor}')
+print(f'Maior valor: {maior_valor}')
+print(f'Média aritmética: {media_aritmetica}')
 print('Fim do programa.')
