df5f2cb332bb44081966d27aacbd9ee8717de4cf
pedro paulo <pedro.ferreira2@escolar.ifrn.edu.br>
2024-08-25 13:37:31 -0300

    questão 6 protótipo

--- lista04.py
+++ lista04.py
@@ -120,3 +120,21 @@ print(f'Menor valor: {menor_valor}')
 print(f'Maior valor: {maior_valor}')
 print(f'Média aritmética: {media_aritmetica}')
 print('Fim do programa.')
+
+#questão06 (precisa de uns ajustes)
+lista_numeros = []
+
+for x in range(3):
+    numeros = int(input("insira 3 números inteiros:"))
+    lista_numeros.append(numeros)
+texto = str(input("insira uma string com 3 caractéres:"))
+if len(texto) != 3:
+    print("erro, a quantidade de caracteres da string deve ser 3")
+else:
+    for item in range(len(lista_numeros)):
+        if item%2 != 0:
+            lista_numeros[item] = texto[item]
+for item in lista_numeros:
+    print(item, end=' ')
+print("fim do programa")
+
--- teste_ray.py
+++ teste_ray.py
@@ -1,2 +1,19 @@
+lista_numeros = []
+
+for x in range(3):
+    numeros = int(input("insira 3 números inteiros:"))
+    lista_numeros.append(numeros)
+texto = str(input("insira uma string com 3 caractéres:"))
+if len(texto) != 3:
+    print("erro, a quantidade de caracteres da string deve ser 3")
+else:
+    for item in range(len(lista_numeros)):
+        if item%2 != 0:
+            lista_numeros[item] = texto[item]
+for item in lista_numeros:
+    print(item, end=' ')
+
+
+    
 
 
