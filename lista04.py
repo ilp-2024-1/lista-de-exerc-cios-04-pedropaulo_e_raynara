7fafe05bf09685fc281f318629c1fd125260af6e
Raynara <raynara.gustavo@escolar.ifrn.edu.br>
2024-08-28 21:08:16 -0300

    questão 10

--- lista04.py
+++ lista04.py
@@ -171,5 +171,24 @@ for palavra, quantidade in contagem.items():
     resultado += f"{palavra}={quantidade}; "
 
 print(resultado)
-print("fim do programa.")
+print("Fim do programa.")
 
+#questão10
+linhas = 3
+colunas = 3
+matriz = []
+valores_impares = 0
+
+for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+        valor = int(input(f"Digite o valor para a posição ({i+1}, {j+1}): "))
+        linha += [valor]
+        if valor % 2 != 0:
+            valores_impares += 1 
+    matriz += [linha]
+
+for linha in matriz:
+    print(linha)
+print(f'Quantidade de números ímpares: {valores_impares}')
+print("Fim do programa.")
--- teste_ray.py
+++ teste_ray.py
@@ -1,24 +1,20 @@
-frase = str(input("escreva uma frase:"))
+linhas = 3
+colunas = 3
+matriz = []
+valores_impares = 0
 
-sinais_pontuacao = ".,!?;:()[]}{\"'"
-for sinal in sinais_pontuacao:
-    frase = frase.replace(sinal, ' ')
-palavras = frase.split()
-print(palavras)
-contagem = {} 
-for palavra in palavras:
-    if palavra in contagem:
-        contagem[palavra] += 1
-    else:
-        contagem[palavra] = 1
-resultado = ""
-for palavra, quantidade in contagem.items():
-    resultado += f"{palavra}={quantidade}; "
+for i in range(linhas):
+    linha = []
+    for j in range(colunas):
+        valor = int(input(f"Digite o valor para a posição ({i+1}, {j+1}): "))
+        linha += [valor]
+        if valor % 2 != 0:
+            valores_impares += 1 
+    matriz += [linha]
 
-print(resultado)
-    
-
-
+for linha in matriz:
+    print(linha)
+print(f'Quantidade de números ímpares: {valores_impares}')
 
 
     
